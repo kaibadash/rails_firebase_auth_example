344ad0adc4129be4127723597f137faabbbe8642
kaiba <kaibadash@gmail.com>
2021-05-01 13:00:39 +0900

    Show firebase auth in Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import initFirebase from "../services/Firebase";
 
-function Login() { 
-  return (
-    <div className="App">
-      Login
-    </div>
-  );
+class Login extends React.Component<{}> {
+  constructor(props: {}) {
+    super(props);
+    initFirebase();
+  }
+  render() {
+    return (
+      <div className="App">
+        Login
+        <div id="firebaseui-auth-container"></div>
+      </div>
+    );
+  }
 }
 
 export default Login;
